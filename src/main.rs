cf5b0cb756e94e71f664b52e5f1931a0bfb31fe2
Emily Eisenberg <xymostech@gmail.com>
2016-03-13 08:15:42 -0700

    Vec/struct cleanup

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,64 @@ use std::io::BufReader;
 use std::io::BufRead;
 use std::cmp;
 
-struct Point(f32, f32, f32);
-#[derive(Clone)]
-struct FacePoint(usize, usize, usize);
+#[derive(Clone, Copy)]
+struct Vec2<T> {
+    x: T,
+    y: T,
+}
+
+impl <T: Copy + Ord> Vec2<T> {
+    fn clamp(&self, min: &Vec2<T>, max: &Vec2<T>) -> Vec2<T> {
+        Vec2::<T> {
+            x: cmp::max(min.x, cmp::min(max.x, self.x)),
+            y: cmp::max(min.y, cmp::min(max.y, self.y)),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Vec3<T> {
+    x: T,
+    y: T,
+    z: T,
+}
+
+impl <T: Copy + std::ops::Mul<T, Output = T> + std::ops::Sub<T, Output = T>> Vec3<T> {
+    fn cross(&self, other: &Vec3<T>) -> Vec3<T> {
+        Vec3::<T> {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+struct FacePoint {
+    vindex: usize,
+    tindex: usize,
+    nindex: usize,
+}
 struct Face(FacePoint, FacePoint, FacePoint);
 
 struct Obj {
-    verts: Vec<Point>,
-    tex_verts: Vec<Point>,
-    norm_verts: Vec<Point>,
+    verts: Vec<Vec3<f32>>,
+    tex_verts: Vec<Vec3<f32>>,
+    norm_verts: Vec<Vec3<f32>>,
     faces: Vec<Face>,
 }
 
-fn parse_point(line: &String) -> Point {
+fn parse_point(line: &String) -> Vec3<f32> {
     let mut iter = line.split_whitespace();
 
     iter.next();
     let vec: Vec<f32> = iter.map(|x| x.parse().unwrap()).collect();
 
-    Point(vec[0], vec[1], vec[2])
+    Vec3::<f32> { x: vec[0], y: vec[1], z: vec[2] }
 }
 
 impl Obj {
-    fn vert(&self, i: usize) -> &Point {
+    fn vert(&self, i: usize) -> &Vec3<f32> {
         &self.verts[i - 1]
     }
 
@@ -62,10 +97,10 @@ impl Obj {
                 for face in vec {
                     let vec: Vec<usize> = face.split("/").map(|x| x.parse().unwrap()).collect();
 
-                    face_vec.push(FacePoint(vec[0], vec[1], vec[2]));
+                    face_vec.push(FacePoint { vindex: vec[0], tindex: vec[1], nindex: vec[2] });
                 }
 
-                obj.faces.push(Face(face_vec[0].clone(), face_vec[1].clone(), face_vec[2].clone()));
+                obj.faces.push(Face(face_vec[0], face_vec[1], face_vec[2]));
             }
         }
 
@@ -170,29 +205,18 @@ fn line(x0: usize, y0: usize, x1: usize, y1: usize, image: &mut Image, color: &C
 }
 
 fn face_line(obj: &Obj, p1: &FacePoint, p2: &FacePoint, image: &mut Image, color: &Color) {
-    let v1 = &obj.verts[p1.0 - 1];
-    let v2 = &obj.verts[p2.0 - 1];
+    let v1 = obj.vert(p1.vindex);
+    let v2 = obj.vert(p2.vindex);
 
-    let x0 = (v1.0 + 1.0) * 400.0;
-    let x1 = (v2.0 + 1.0) * 400.0;
-    let y0 = (v1.1 + 1.0) * 400.0;
-    let y1 = (v2.1 + 1.0) * 400.0;
+    let x0 = (v1.x + 1.0) * 400.0;
+    let x1 = (v2.x + 1.0) * 400.0;
+    let y0 = (v1.y + 1.0) * 400.0;
+    let y1 = (v2.y + 1.0) * 400.0;
 
     line(x0 as usize, y0 as usize, x1 as usize, y1 as usize, image, color);
 }
 
-struct Vec2 {
-    x: isize,
-    y: isize,
-}
-
-struct Vec3 {
-    x: isize,
-    y: isize,
-    z: isize,
-}
-
-fn bounding_box(points: &[&Vec2]) -> (Vec2, Vec2) {
+fn bounding_box(points: &[&Vec2<isize>]) -> (Vec2<isize>, Vec2<isize>) {
     let mut xmin = isize::max_value();
     let mut ymin = isize::max_value();
     let mut xmax = isize::min_value();
@@ -205,25 +229,13 @@ fn bounding_box(points: &[&Vec2]) -> (Vec2, Vec2) {
         ymax = cmp::max(ymax, point.y);
     }
 
-    (Vec2 { x: xmin, y: ymin }, Vec2 { x: xmax, y: ymax })
+    (Vec2::<isize> { x: xmin, y: ymin }, Vec2::<isize> { x: xmax, y: ymax })
 }
 
-fn clamp(v: &Vec2, min: &Vec2, max: &Vec2) -> Vec2 {
-    Vec2 { x: cmp::max(min.x, cmp::min(max.x, v.x)), y: cmp::max(min.y, cmp::min(max.y, v.y)) }
-}
-
-fn cross(a: &Vec3, b: &Vec3) -> Vec3 {
-    Vec3 {
-        x: a.y * b.z - a.z * b.y,
-        y: a.z * b.x - a.x * b.z,
-        z: a.x * b.y - a.y * b.x,
-    }
-}
-
-fn inside_triangle(p: &Vec2, t0: &Vec2, t1: &Vec2, t2: &Vec2) -> bool {
-    let c = cross(
-        &Vec3 { x: t2.x - t0.x, y: t1.x - t0.x, z: t0.x - p.x },
-        &Vec3 { x: t2.y - t0.y, y: t1.y - t0.y, z: t0.y - p.y }
+fn inside_triangle(p: &Vec2<isize>, t0: &Vec2<isize>, t1: &Vec2<isize>, t2: &Vec2<isize>) -> bool {
+    let c = Vec3::cross(
+        &Vec3::<isize> { x: t2.x - t0.x, y: t1.x - t0.x, z: t0.x - p.x },
+        &Vec3::<isize> { x: t2.y - t0.y, y: t1.y - t0.y, z: t0.y - p.y }
     );
 
     if c.z < 0 {
@@ -237,15 +249,15 @@ fn inside_triangle(p: &Vec2, t0: &Vec2, t1: &Vec2, t2: &Vec2) -> bool {
     }
 }
 
-fn triangle(t0: &Vec2, t1: &Vec2, t2: &Vec2, image: &mut Image, color: &Color) {
+fn triangle(t0: &Vec2<isize>, t1: &Vec2<isize>, t2: &Vec2<isize>, image: &mut Image, color: &Color) {
     let (bbmin, bbmax) = bounding_box(&vec![t0, t1, t2]);
-    let min = Vec2 { x: 0, y: 0 };
-    let max = Vec2 { x: (image.width - 1) as isize, y: (image.height - 1) as isize };
-    let (bbmin, bbmax) = (clamp(&bbmin, &min, &max), clamp(&bbmax, &min, &max));
+    let min = Vec2::<isize> { x: 0, y: 0 };
+    let max = Vec2::<isize> { x: (image.width - 1) as isize, y: (image.height - 1) as isize };
+    let (bbmin, bbmax) = (Vec2::clamp(&bbmin, &min, &max), Vec2::clamp(&bbmax, &min, &max));
 
     for x in bbmin.x as usize..bbmax.x as usize {
         for y in bbmin.y as usize..bbmax.y as usize {
-            if inside_triangle(&Vec2 { x: x as isize, y: y as isize }, t0, t1, t2) {
+            if inside_triangle(&Vec2::<isize> { x: x as isize, y: y as isize }, t0, t1, t2) {
                 image.set_pixel(x, y, color);
             }
         }
@@ -261,13 +273,13 @@ fn main() {
     let mut color = 0;
 
     for face in obj.faces.iter() {
-        let fp0 = obj.vert((face.0).0);
-        let fp1 = obj.vert((face.1).0);
-        let fp2 = obj.vert((face.2).0);
+        let fp0 = obj.vert(face.0.vindex);
+        let fp1 = obj.vert(face.1.vindex);
+        let fp2 = obj.vert(face.2.vindex);;
 
-        let t0 = Vec2 { x: ((fp0.0 + 1.0) * 400.0) as isize, y: ((fp0.1 + 1.0) * 400.0) as isize };
-        let t1 = Vec2 { x: ((fp1.0 + 1.0) * 400.0) as isize, y: ((fp1.1 + 1.0) * 400.0) as isize };
-        let t2 = Vec2 { x: ((fp2.0 + 1.0) * 400.0) as isize, y: ((fp2.1 + 1.0) * 400.0) as isize };
+        let t0 = Vec2::<isize> { x: ((fp0.x + 1.0) * 400.0) as isize, y: ((fp0.y + 1.0) * 400.0) as isize };
+        let t1 = Vec2::<isize> { x: ((fp1.x + 1.0) * 400.0) as isize, y: ((fp1.y + 1.0) * 400.0) as isize };
+        let t2 = Vec2::<isize> { x: ((fp2.x + 1.0) * 400.0) as isize, y: ((fp2.y + 1.0) * 400.0) as isize };
 
         triangle(&t0, &t1, &t2, &mut image, &colors[color % 4]);
         color += 1;
